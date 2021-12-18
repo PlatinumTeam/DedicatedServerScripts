1ed0aa883b69acae60ed807b78bd724350370551
HiGuy <higuymb@gmail.com>
2021-12-18 18:41:56 -0500

    Some extra goodies from Tsuf (rip)

---
+++ tsuf/start-server.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+cd /home/marbleblast/pq/scripts/
+su -c ./start-server.sh marbleblast
+
---
+++ tsuf/stop-server.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+cd /home/marbleblast/pq/scripts/
+su -c ./stop-server.sh marbleblast
+
